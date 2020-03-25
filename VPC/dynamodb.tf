eb31bdf71d7bb548e08933555d6efd259fe74ca6
umar832 <umar@ip-172-31-90-247.ec2.internal>
2020-03-25 17:55:08 +0000

    add

--- VPC/dynamodb.tf
+++ VPC/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
+resource "aws_dynamodb_table" "basic-dynamodb-table1" {
   name           = "dynamodb"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
