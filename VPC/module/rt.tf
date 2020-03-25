d2d446434fdba64a589ff1b185e4d5f2bf7a9265
Cloud User <centos@ip-172-31-20-251.eu-west-2.compute.internal>
2020-03-25 22:29:36 +0000

    new

--- VPC/module/rt.tf
+++ VPC/module/rt.tf
@@ -31,15 +31,15 @@ resource "aws_route_table" "nat_gw" {
 
 resource "aws_route_table_association" "dev_private1" { 
   subnet_id = "${aws_subnet.dev_private1.id}" 
-  route_table_id = "${aws_route_table.devforpriv.id}" 
+  route_table_id = "${aws_route_table.nat_gw.id}" 
 } 
 
 resource "aws_route_table_association" "dev_private2" { 
   subnet_id = "${aws_subnet.dev_private2.id}" 
-  route_table_id = "${aws_route_table.devforpriv.id}" 
+  route_table_id = "${aws_route_table.nat_gw.id}" 
 } 
 
 resource "aws_route_table_association" "dev_private3" { 
   subnet_id = "${aws_subnet.dev_private3.id}" 
-  route_table_id = "${aws_route_table.devforpriv.id}" 
+  route_table_id = "${aws_route_table.nat_gw.id}" 
 } 
