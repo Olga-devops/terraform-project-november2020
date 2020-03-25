d55a6fd65e88f4e602d64a6f1af30e9ad2ab1a24
umar832 <umar@ip-172-31-90-247.ec2.internal>
2020-03-25 14:22:12 +0000

    add

--- VPC/provider.tf
+++ VPC/provider.tf
@@ -1,7 +1,3 @@
-provider "aws" { 
-  region = "${var.region}"
-} 
-
 provider "aws" {
   region = "us-east-1"
   alias  =  "virginia"
--- VPC/s3.tf
+++ VPC/s3.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "b" {
   bucket = "terraform-project-backend-2020"
 
 
-
   tags = {
     Name        = "My bucket"
     Environment = "dev"
