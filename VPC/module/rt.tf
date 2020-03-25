b28210398f7d90732df06f642e822066aed6304f
Cloud User <centos@ip-172-31-20-251.eu-west-2.compute.internal>
2020-03-25 22:21:32 +0000

    1

--- VPC/module/rt.tf
+++ VPC/module/rt.tf
@@ -25,7 +25,7 @@ resource "aws_route_table" "devforpriv" {
   vpc_id = "${aws_vpc.dev.id}" 
   route { 
     cidr_block = "0.0.0.0/0" 
-    gateway_id = "${aws_nat_gateway.dev.id}" 
+    nat_gateway_id = "${aws_nat_gateway.dev.id}" 
     } 
 }
 
