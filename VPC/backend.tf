7907eadfdc89a621126f3cbd74f968162b1388a6
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-03-25 14:46:43 -0500

    Update backend.tf

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -3,6 +3,6 @@ backend "s3" {
 bucket = "terraform-project-backend-2020"
 key = "tower/us-east-1/tools/virginia/tower.tfstate"
 region = "us-east-1"
-#dynamodb = "terraform-team1"
+dynamodb_table = "terraform-team1"
   }
 }
