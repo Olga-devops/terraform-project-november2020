fd3959a4c6ba09c175d38adaff9420db7982749a
Olga-devops <59778880+Olga-devops@users.noreply.github.com>
2020-07-04 15:29:59 -0500

    Update subnet.tf
    added auto assign public ip to public subnets

--- VPC/module/subnet.tf
+++ VPC/module/subnet.tf
@@ -3,18 +3,21 @@ resource "aws_subnet" "dev1" {
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block1_public}" 
   availability_zone = "${var.az1}"
+  map_public_ip_on_launch = true
 } 
 
 resource "aws_subnet" "dev2" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block2_public}"
   availability_zone = "${var.az2}"
+  map_public_ip_on_launch = true
 } 
 
 resource "aws_subnet" "dev3" { 
   vpc_id = "${aws_vpc.dev.id}" 
   cidr_block = "${var.cidr_block3_public}"
   availability_zone = "${var.az3}"
+  map_public_ip_on_launch = true
 } 
 
 # Private subnets
